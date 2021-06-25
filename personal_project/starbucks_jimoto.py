b89c484d641c644a90b783e237273af966bac79d
Janice Koh <zapjanice@gmail.com>
2021-06-26 00:15:21 +0900

    Added code to get images

--- personal_project/starbucks_jimoto.py
+++ personal_project/starbucks_jimoto.py
@@ -2,10 +2,13 @@
 Please see the url below for more information'''
 
 import requests
+import time
+import urllib.request
 import pandas as pd
 from bs4 import BeautifulSoup
 
-url = "https://www.starbucks.co.jp/cafe/jimoto_frappuccino/"
+base = "https://www.starbucks.co.jp"
+url = base + "/cafe/jimoto_frappuccino/"
 response = requests.get(url)
 soup = BeautifulSoup(response.content, "html.parser")
 
@@ -14,15 +17,23 @@ items = []
 for item in soup.find("ul", class_="_list").find_all("li"):
     prefecture = item.find("a").select('img')[0]['alt']
     end_link = item.find("a").get('href')
-    link = "https://www.starbucks.co.jp"+end_link
+    link = base+end_link
     response_2 = requests.get(link)
     soup_2 = BeautifulSoup(response_2.content, "html.parser")
     flavor = soup_2.find("p", class_="_t2").text
-    items.append({'prefecture': prefecture, 'flavor': flavor, 'link': link})
+    img_link = item.find('img', src=True)
+    img_link = base + (img_link["src"].split("src=")[-1])   
+    items.append({'prefecture': prefecture, 'flavor': flavor, 'link': link, 'img_link':img_link})
+    
+    timestamp = time.asctime() 
+    txt = open(f'{prefecture}.png', "wb")
+    download_img = urllib.request.urlopen(img_link)
+    txt.write(download_img.read())
+    txt.close()      
 
 '''Using pandas to clean data'''
 df = pd.DataFrame(items)
 df['prefecture'] = df['prefecture'].replace({'#': ''},regex=True)
-df['prefecture'] = df['prefecture'].str.replace('\d+', '')
-
+df['prefecture'] = df['prefecture'].str.replace('\d+', '', regex=True)
+df['prefecture'] = df['prefecture'].str.lstrip()
 df.to_csv('starbucks_jimoto.csv', index=False)
